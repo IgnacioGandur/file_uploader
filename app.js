9edc396f640d4609f48cbe36fc391c23dc6f084f
Ignacio Gandur <gandurjuanignacio@gmail.com>
2025-01-31 11:05:57 -0300

    Added Prisma session store

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import "dotenv/config";
 import express from "express";
 import path from "path";
 import router from "./routes/router.js";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
 
 const __dirname = import.meta.dirname;
 const app = express();
@@ -13,6 +16,23 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
+
+// Sessions
+app.use(
+    session({
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 30, // 1 month cookie.
+        },
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: new PrismaSessionStore(new PrismaClient(), {
+            checkPeriod: 1000 * 60 * 3,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }),
+    }),
+);
 
 app.use(router);
 
