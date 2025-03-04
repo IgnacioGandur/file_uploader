32bf4772b9676d968a6495e92da1196259f16241
Ignacio Gandur <gandurjuanignacio@gmail.com>
2025-03-04 08:02:43 -0300

    Style: advanced in the styling of folder-details page.

--- routes/updateFolderRouter.js
+++ routes/updateFolderRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import updateFolderController from "../controllers/updateFolderController.js";
+import validateFolderNameUpdate from "../validators/folder/validateFolderNameUpdate.js";
 
 const updateFolderRouter = Router();
 
 updateFolderRouter.post(
     "/:folderName",
+    validateFolderNameUpdate,
     updateFolderController.updateFolderPost,
 );
 
