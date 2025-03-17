b89069f6acbb299f3642db21044c52f9f3422b00
Ignacio Gandur <gandurjuanignacio@gmail.com>
2025-03-17 01:02:41 -0300

    Test: added Cloudinary config to the interactions file.

--- cloudinary/cloudinaryInteractions.js
+++ cloudinary/cloudinaryInteractions.js
@@ -1,6 +1,14 @@
 import { v2 as cloudinary } from "cloudinary";
 import createFileUri from "../utilities/createFileUri.js";
 
+(async () => {
+    cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET,
+    });
+})();
+
 const cloudinaryInteractions = {
     createUserFolder: (username) => {
         cloudinary.api.create_folder(username);
